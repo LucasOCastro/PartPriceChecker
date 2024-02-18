2c950f22063893646d5985ca5273aee0997ffa54
lucas castro <lucas_o__castro@hotmail.com>
2024-02-18 02:40:15 -0300

    Observable model class

---
+++ PriceChecker2/ObservableViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PriceChecker2;
+
+public abstract class ObservableModelView : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void SetValue<T>(ref T _property, T value, [CallerMemberName] string name = "")
+    {
+        _property = value;
+        PropertyChanged?.Invoke(this, new(name));
+    }
+}
---
+++ PriceChecker2/Part.cs
@@ -0,0 +1,7 @@
+﻿namespace PriceChecker2;
+
+public class Part
+{
+    public string Name { get; set; } = "New Part";
+    public string[] Urls { get; set; } = Array.Empty<string>();
+}
