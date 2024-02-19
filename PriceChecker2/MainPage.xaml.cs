06b5e7b2b0461d558d3cb1d087b996fc51c19244
lucas castro <lucas_o__castro@hotmail.com>
2024-02-19 15:10:07 -0300

    Part display rework and move to correct page

--- PriceChecker2/MainPage.xaml.cs
+++ PriceChecker2/MainPage.xaml.cs
@@ -5,22 +5,8 @@ namespace PriceChecker2;
 
 public partial class MainPage : ContentPage
 {
-    private readonly ObservableCollection<PartInfo> _scrapedParts = new();
-
-    private async Task LoadPartInfoAsync(IEnumerable<Part> parts)
-    {
-        var scraped = parts.Select(p => new PartInfo(p)).ToList();
-        while (scraped.Any(s => s.Loading))
-            await Task.Delay(50);
-
-        foreach (var s in scraped)
-            _scrapedParts.Add(s);
-    }
-
     public MainPage()
     {
         InitializeComponent();
-        _partsCollectionView.ItemsSource = _scrapedParts;
-        LoadPartInfoAsync(PartDatabase.Instance.Parts);
     }
 }
--- PriceChecker2/PartTrackerPage.xaml.cs
+++ PriceChecker2/PartTrackerPage.xaml.cs
@@ -1,9 +1,25 @@
+using System.Collections.ObjectModel;
+
 namespace PriceChecker2;
 
 public partial class PartTrackerPage : ContentPage
 {
-	public PartTrackerPage()
+    private readonly ObservableCollection<PartInfo> _scrapedParts = new();
+
+    private async Task LoadPartInfoAsync(IEnumerable<Part> parts)
+    {
+        var scraped = parts.Select(p => new PartInfo(p)).ToList();
+        while (scraped.Any(s => s.Loading))
+            await Task.Delay(50);
+
+        foreach (var s in scraped)
+            _scrapedParts.Add(s);
+    }
+
+    public PartTrackerPage()
 	{
 		InitializeComponent();
-	}
+        _partsCollectionView.ItemsSource = _scrapedParts;
+        Task.Run(() => LoadPartInfoAsync(PartDatabase.Instance.Parts));
+    }
 }
