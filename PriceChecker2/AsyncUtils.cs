99c3fdb58030b835bca65dd3ce21cb3f7427aa17
lucas castro <lucas_o__castro@hotmail.com>
2024-02-19 22:45:23 -0300

    Async Utils with WaitWhile and WaitUntil

---
+++ PriceChecker2/AsyncUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace PriceChecker2;
+
+public static class AsyncUtils
+{
+    public static async Task WaitUntil(Func<bool> predicate, int ms = 50)
+    {
+        while (!predicate())
+            await Task.Delay(ms);
+    }
+
+    public static async Task WaitWhile(Func<bool> predicate, int ms = 50)
+    {
+        while (predicate())
+            await Task.Delay(ms);
+    }
+}
