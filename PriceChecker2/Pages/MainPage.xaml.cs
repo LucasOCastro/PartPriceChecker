f90873c7256ac66f2d155f90aaca3c065c923bbe
lucas castro <lucas_o__castro@hotmail.com>
2024-02-19 23:56:18 -0300

    Main Page displays BuildPart parts

--- PriceChecker2/Pages/MainPage.xaml.cs
+++ PriceChecker2/Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using PriceChecker2.Parts;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace PriceChecker2;
 
@@ -8,18 +10,22 @@ public partial class MainPage : ContentPage
     {
         BindingContext = this;
         InitializeComponent();
+        LoadPartInfoAsync();
     }
 
+    private async Task LoadPartInfoAsync()
+    {
+        IsBusy = true;
+        await AsyncUtils.WaitUntil(() => PartDatabase.Instance.AllLoaded);
+        _partsCollectionView.ItemsSource = PartDatabase.Instance.Parts;
+        IsBusy = false;
+    }
 
+    private void ClearButton_Pressed(object sender, EventArgs e) => ClearAsync();
     private async Task ClearAsync()
     {
         IsBusy = true;
         await PartDatabase.Instance.ClearAsync();
         IsBusy = false;
-    }
-    
-    private void ClearButton_Pressed(object sender, EventArgs e)
-    {
-        ClearAsync();
     }
 }
