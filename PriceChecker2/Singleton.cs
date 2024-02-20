99d826541b842d8ae1b917805d0d6f0482b102b7
lucas castro <lucas_o__castro@hotmail.com>
2024-02-19 22:44:16 -0300

    Singleton instantiated on static initialization

--- PriceChecker2/Singleton.cs
+++ PriceChecker2/Singleton.cs
@@ -2,6 +2,6 @@
 
 public abstract class Singleton<T> where T : Singleton<T>, new()
 {
-    private static T? _instance;
-    public static T Instance => _instance ??= new();
+    private static readonly T _instance = new();
+    public static T Instance => _instance;
 }
