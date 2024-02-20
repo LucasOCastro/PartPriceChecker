8ba8752b855baab240168d04f01b546008197083
lucas castro <lucas_o__castro@hotmail.com>
2024-02-19 22:45:36 -0300

    PartDatabase now exposes PartInfo instead of Part

--- PriceChecker2/Pages/PartTrackerPage.xaml.cs
+++ PriceChecker2/Pages/PartTrackerPage.xaml.cs
@@ -1,21 +1,14 @@
-using System.Collections.ObjectModel;
 using PriceChecker2.Parts;
 
 namespace PriceChecker2;
 
 public partial class PartTrackerPage : ContentPage
 {
-    private readonly ObservableCollection<PartInfo> _scrapedParts = new();
-
-    private async Task LoadPartInfoAsync(IEnumerable<Part> parts)
+    private async Task LoadPartInfoAsync()
     {
         IsBusy = true;
-        var scraped = parts.Select(p => new PartInfo(p)).ToList();
-        while (scraped.Any(p => p.Loading))
-            await Task.Delay(50);
-
-        foreach (var s in scraped)
-            _scrapedParts.Add(s);
+        await AsyncUtils.WaitUntil(() => PartDatabase.Instance.AllLoaded);
+        _partsCollectionView.ItemsSource = PartDatabase.Instance.Parts;
         IsBusy = false;
     }
 
@@ -23,8 +16,6 @@ public partial class PartTrackerPage : ContentPage
 	{
 		InitializeComponent();
         BindingContext = this;
-        _partsCollectionView.ItemsSource = _scrapedParts;
-
-        LoadPartInfoAsync(PartDatabase.Instance.Parts);
+        LoadPartInfoAsync();
     }
 }
--- PriceChecker2/Parts/PartDatabase.cs
+++ PriceChecker2/Parts/PartDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using CommunityToolkit.Maui.Core.Extensions;
+using System.Collections.ObjectModel;
 using System.Text.Json;
 
 namespace PriceChecker2.Parts;
@@ -10,9 +11,11 @@ public class PartDatabase : Singleton<PartDatabase>
 
     private bool _busy;
 
-    private readonly ObservableCollection<Part> _parts;
-    public ReadOnlyObservableCollection<Part> Parts { get; }
+    private readonly ObservableCollection<PartInfo> _partInfos;
+    public ReadOnlyObservableCollection<PartInfo> Parts { get; }
+    public bool AllLoaded => !Parts.Any(p => p.Loading);
 
+    private readonly List<Part> _parts;
     public PartDatabase()
     {
         if (!File.Exists(Dir))
@@ -20,18 +23,16 @@ public class PartDatabase : Singleton<PartDatabase>
             _parts = new();
             File.WriteAllText(Dir, JsonSerializer.Serialize(_parts));
         }
-        else _parts = new ObservableCollection<Part>(JsonSerializer.Deserialize<List<Part>>(File.ReadAllText(Dir)) ?? new());
-        Parts = new(_parts);
+        else _parts = JsonSerializer.Deserialize<List<Part>>(File.ReadAllText(Dir)) ?? new();
+        
+        _partInfos = _parts.Select(p => new PartInfo(p)).ToObservableCollection();
+        Parts = new(_partInfos);
     }
 
     public async Task SaveChangesAsync()
     {
         if (_busy)
-            await new Task(async () =>
-            {
-                while (_busy)
-                    await Task.Delay(100);
-            });
+            await AsyncUtils.WaitWhile(() => _busy);
 
         _busy = true;
         await File.WriteAllTextAsync(Dir, JsonSerializer.Serialize(_parts));
@@ -41,18 +42,22 @@ public class PartDatabase : Singleton<PartDatabase>
     public async Task RegisterAsync(Part part)
     {
         _parts.Add(part);
+        _partInfos.Add(new(part));
         await SaveChangesAsync();
     }
 
     public async Task UnregisterAsync(Part part)
     {
-        _parts.Remove(part);
+        int index = _parts.IndexOf(part);
+        _parts.RemoveAt(index);
+        _partInfos.RemoveAt(index);
         await SaveChangesAsync();
     }
 
     public async Task ClearAsync()
     {
         _parts.Clear();
+        _partInfos.Clear();
         await SaveChangesAsync();
     }
 }
