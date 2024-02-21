48e4764d11d7824a4bcb23cf0d7627c741f16cfa
lucas castro <lucas_o__castro@hotmail.com>
2024-02-21 19:44:13 -0300

    A few IList functions for sorted insertion and observable swap

--- PriceChecker2/CollectionUtils.cs
+++ PriceChecker2/CollectionUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,5 +20,45 @@ public static class CollectionUtils
         bool contains = dict.ContainsKey(key);
         if (contains) dict[key] = value;
         return contains;
+    }
+
+    public static void Swap<T>(this ObservableCollection<T> col, int a, int b)
+    {
+        if (a == b) return;
+        if (a > b) (a, b) = (b, a);
+
+        col.Move(a, b);
+        col.Move(b - 1, a);
+    }
+    public static int BinarySearchIndexOf<T>(this IList<T> list, T obj, Comparison<T> comparison) 
+        => list.BinarySearchIndexOf(obj, Comparer<T>.Create(comparison));
+    public static int BinarySearchIndexOf<T>(this IList<T> list, T obj, IComparer<T>? comparer = null)
+    {
+        comparer ??= Comparer<T>.Default;
+
+        int min = 0;
+        int max = list.Count - 1;
+        while (min <= max)
+        {
+            int mid = min + (max - min) / 2;
+            int comp = comparer.Compare(obj, list[mid]);
+
+            if (comp < 0) 
+                max = mid - 1;
+            else if (comp > 0) 
+                min = mid + 1;
+            else return mid;
+        }
+        return ~min;
+    }
+
+    public static void InsertOrderedBy<T, TKey>(this IList<T> list, T obj, Func<T, TKey> keySelector) where TKey : IComparable
+        => list.InsertOrdered(obj, (a, b) => keySelector(a).CompareTo(b));
+    public static void InsertOrdered<T>(this IList<T> list, T obj, Comparison<T> comparison)
+        => list.InsertOrdered(obj, Comparer<T>.Create(comparison));
+    public static void InsertOrdered<T>(this IList<T> list, T obj, IComparer<T> comparer)
+    {
+        int i = list.BinarySearchIndexOf(obj, comparer);
+        list.Insert(i, obj);
     }
 }
