87250b57fb7c5deb1661b7cce8b3a245e320b19e
lucas castro <lucas_o__castro@hotmail.com>
2024-02-21 03:46:11 -0300

    Main page with money and new Build class

--- PriceChecker2/Pages/MainPage.xaml.cs
+++ PriceChecker2/Pages/MainPage.xaml.cs
@@ -1,12 +1,11 @@
 ﻿using PriceChecker2.Parts;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 
 namespace PriceChecker2.Pages;
 
 public partial class MainPage : ContentPage
 {
-    private readonly ObservableCollection<PartInfo> _buildParts = new();
+    public Build Build { get; } = new();
+    public string MoneyString { get; set; }
 
     public MainPage()
     {
@@ -19,39 +18,8 @@ public partial class MainPage : ContentPage
         IsBusy = true;
 
         await AsyncUtils.WaitUntil(() => PartDatabase.Instance.AllLoaded);
-
-        foreach (var part in PartDatabase.Instance.Parts)
-        {
-            if (part.IsBuildPart)
-                _buildParts.Add(part);
-            part.PropertyChanged += OnPartPropertyChanged;
-        }
-        _collectionView.ItemsSource = _buildParts;
-
-        PartDatabase.Instance.OnPartRegistered += OnPartRegistered;
-        PartDatabase.Instance.OnPartUnregistered += OnPartUnregistered;
+        //_collectionView.ItemsSource = Build.BuildParts;
 
         IsBusy = false;
-    }
-
-    private void OnPartRegistered(PartInfo part)
-    {
-        if (part.IsBuildPart) _buildParts.Add(part);
-        part.PropertyChanged += OnPartPropertyChanged;
-    }
-
-    private void OnPartUnregistered(PartInfo part)
-    {
-        if (part.IsBuildPart) _buildParts.Remove(part);
-        part.PropertyChanged -= OnPartPropertyChanged;
-    }
-
-    private void OnPartPropertyChanged(object? sender, EventArgs e)
-    {
-        if (sender is not PartInfo part) return;
-        if (e is not PropertyChangedEventArgs { PropertyName: nameof(PartInfo.IsBuildPart)}) return;
-
-        if (part.IsBuildPart) _buildParts.Add(part);
-        else _buildParts.Remove(part);
     }
 }
