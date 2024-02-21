f412f68b4b322bbbf274ffdb1ab34c6c993ebcb9
lucas castro <lucas_o__castro@hotmail.com>
2024-02-21 14:37:45 -0300

    Show percentages in MainPage

--- PriceChecker2/Pages/MainPage.xaml.cs
+++ PriceChecker2/Pages/MainPage.xaml.cs
@@ -15,13 +15,38 @@ public partial class MainPage : ContentPage
             _moneyString = value;
             OnPropertyChanged(nameof(MoneyString));
             CalculateAffordables();
+            UpdatePercentage();
         }
     }
+
+    private bool TryGetMoneyValue(out double money)
+    {
+        string moneyString = MoneyString;
+        if (string.IsNullOrWhiteSpace(moneyString)) moneyString = "0";
+        return double.TryParse(moneyString, out money);
+    }
+
+    private string _percentageString;
+    public string PercentageString
+    {
+        get => _percentageString;
+        set
+        {
+            _percentageString = value;
+            OnPropertyChanged(nameof(PercentageString));
+        }
+    }
+
+
 
     public MainPage()
     {
         InitializeComponent();
         LoadPartInfoAsync();
+        Build.PropertyChanged += (s, e) => {
+            if (e.PropertyName == nameof(Build.TotalValidPrice))
+                UpdatePercentage();
+        };
     }
 
     private async Task LoadPartInfoAsync()
@@ -32,11 +57,31 @@ public partial class MainPage : ContentPage
         IsBusy = false;
     }
 
+    private void UpdatePercentage()
+    {
+        if (!TryGetMoneyValue(out double money)) return;
+
+        if (Build.TotalValidPrice == 0)
+            PercentageString = "100%";
+        else
+        {
+            int percentage = (int)(money * 100f / Build.TotalValidPrice);
+            PercentageString = percentage.ToString() + '%';
+        }
+    }
+
     private void CalculateAffordables()
     {
-        if (!double.TryParse(MoneyString, out double money)) return;
-        foreach (var part in Build.BuildParts.Where(bp => bp.IsValid))
+        if (!TryGetMoneyValue(out double money)) return;
+
+        foreach (var part in Build.BuildParts)
         {
+            if (!part.IsValid)
+            {
+                part.Affordable = false;
+                continue;
+            }
+
             part.Affordable = money >= part.LowestPrice;
             money -= part.LowestPrice;
             if (money < 0) money = 0;
